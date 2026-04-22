b52301160299cf3ee6b9b78c2d65b4653edef374
M7mdAbolila <m7md.abolila.cs1@gmail.com>
2026-04-22 13:37:51 +0200

    init flutter testing repo

---
+++ test/counter_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing/counter.dart';
+
+void main() {
+  group("Test counter start, increment & decrement", () {
+    test('Counter value should be 0', () {
+      final counter = Counter();
+
+      // expect (actual, matcher)
+      // We expect the initial counter value to be 0
+      expect(counter.value, 0);
+    });
+    test('Counter value should be incremented by 1', () {
+      final counter = Counter();
+      counter.increment();
+
+      // expect (actual, matcher)
+      // We expect the counter value to be 1 after incrementing
+      expect(counter.value, 1);
+    });
+
+    test('Counter value should be decremented by 1', () {
+      final counter = Counter();
+      counter.decrement();
+
+      // expect (actual, matcher)
+      // We expect the counter value to be -1 after decrementing
+      expect(counter.value, -1);
+    });
+  });
+}
